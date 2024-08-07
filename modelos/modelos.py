a2706d290052fbcef17b45c99dbd44fc5aed8fd2
João Silva <dneto21@yahoo.com>
2024-08-06 22:30:14 -0300

    Avaliação do Vendedor

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -1,6 +1,7 @@
 import sqlmodel
 from sqlmodel import SQLModel, Field, Session
 from decimal import Decimal
+from datetime import datetime
 
 
 class Item(SQLModel, table=True):
@@ -14,9 +15,17 @@ class Item(SQLModel, table=True):
 
 class Vendedor(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
-  preco: Decimal = Field(max_digits=6, decimal_places=2)
-  cor: str
-  qtde_estoque: int
-  descricao: str
-  imagem: str
+  nome: str
+  email: str
+  telefone: int
+  pais: str
+  cidade: str
+  estado: str
   ativo: bool = True
+
+class AvaliacaoVendedor(SQLModel, table=True):
+  id: int | None = Field(default=None, primary_key=True)
+  imagem: str
+  comentario: str
+  nota: int
+  criado_em: datetime
