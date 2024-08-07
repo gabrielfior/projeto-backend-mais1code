4996ec9c193898562ec4ac39e721b9d837896fd6
João Silva <dneto21@yahoo.com>
2024-08-06 21:53:14 -0300

    Primeira versão da tabela do vendedor

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -11,3 +11,12 @@ class Item(SQLModel, table=True):
   descricao: str
   imagem: str
   ativo: bool = True
+
+class Vendedor(SQLModel, table=True):
+  id: int | None = Field(default=None, primary_key=True)
+  preco: Decimal = Field(max_digits=6, decimal_places=2)
+  cor: str
+  qtde_estoque: int
+  descricao: str
+  imagem: str
+  ativo: bool = True
