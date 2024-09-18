a688cd64be96b7ff4b1802aecd93b35136fb4c31
gabrielfior <fior.gbm@gmail.com>
2024-09-17 22:07:30 -0300

    Pequenas mudancas nas tabelas

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -1,55 +1,61 @@
-import sqlmodel
-from sqlmodel import SQLModel, Field, Session
-from decimal import Decimal
 from datetime import datetime
-from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
+from decimal import Decimal
+
+from sqlmodel import Relationship
+from sqlmodel import SQLModel, Field
 
 
 class Item(SQLModel, table=True):
-  id: int | None = Field(default=None, primary_key=True)
-  preco: Decimal = Field(max_digits=6, decimal_places=2)
-  cor: str
-  qtde_estoque: int
-  descricao: str
-  imagem: str
-  ativo: bool = True
-  
-  avaliacoes: list["AvaliacaoItem"] = Relationship(back_populates="item")
-  
+    id: int | None = Field(default=None, primary_key=True)
+    preco: Decimal = Field(max_digits=6, decimal_places=2)
+    cor: str
+    qtde_estoque: int
+    descricao: str
+    imagem: str
+    ativo: bool = True
 
-class Vendedor(SQLModel, table=True):
-  id: int | None = Field(default=None, primary_key=True)
-  nome: str
-  email: str
-  telefone: int
-  pais: str
-  cidade: str
-  estado: str
-  ativo: bool = True
-  
-  avaliacoes: list["AvaliacaoVendedor"] = Relationship(back_populates="vendedor")
+    avaliacoes: list["AvaliacaoItem"] = Relationship(back_populates="item")
+
+
+class Pessoa(SQLModel):
+    id: int | None = Field(default=None, primary_key=True)
+    nome: str
+    email: str
+    telefone: int
+    pais: str
+    cidade: str
+    estado: str
+    ativo: bool = True
+
+
+class Vendedor(Pessoa, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    avaliacoes: list["AvaliacaoVendedor"] = Relationship(back_populates="vendedor")
 
 
 class AvaliacaoVendedor(SQLModel, table=True):
-  id: int | None = Field(default=None, primary_key=True)
-  imagem: str
-  comentario: str
-  nota: int
-  criado_em: datetime
+    id: int | None = Field(default=None, primary_key=True)
+    imagem: str
+    comentario: str
+    nota: int
+    criado_em: datetime
 
-  vendedor_id: int | None = Field(default=None, foreign_key="vendedor.id")
-  vendedor: Vendedor | None = Relationship(back_populates="avaliacoes")
+    vendedor_id: int | None = Field(default=None, foreign_key="vendedor.id")
+    vendedor: Vendedor | None = Relationship(back_populates="avaliacoes")
 
 
 class AvaliacaoItem(SQLModel, table=True):
-  id: int | None = Field(default=None, primary_key=True)
-  imagem_url: str
-  video_url: str
-  comentario: str
-  nota: int
-  localizacao: str
-  criado_em: datetime
+    id: int | None = Field(default=None, primary_key=True)
+    imagem_url: str
+    video_url: str
+    comentario: str
+    nota: int
+    localizacao: str
+    criado_em: datetime
 
-  item_id: int | None = Field(default=None, foreign_key="item.id")
-  item: Item | None = Relationship(back_populates="avaliacoes")
+    item_id: int | None = Field(default=None, foreign_key="item.id")
+    item: Item | None = Relationship(back_populates="avaliacoes")
 
+
+class Usuario(Pessoa, table=True):
+    id: int | None = Field(default=None, primary_key=True)
